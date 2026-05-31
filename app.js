cc3ee5a9ff806b09e467455e4c38ba11423266b6
LaraLopez10 <larslopez20@gmail.com>
2026-05-31 16:23:31 -0300

    Cambios_Proyecto

--- app.js
+++
@@ -1,8 +0,0 @@
-import { LibraryController } from './src/controllers/LibraryController.js';
-
-// Esperamos a que el HTML termine de cargar en el navegador
-document.addEventListener('DOMContentLoaded', () => {
-    const controller = new LibraryController();
-    controller.init();
-    console.log(" MVP de Biblioteca Web inicializado correctamente.");
-});
