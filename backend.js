2722a0090e7e4597fe15c724b4285a6aea5b905e
LaraLopez10 <larslopez20@gmail.com>
2026-06-08 18:02:19 -0300

    Modificaciones

--- backend.js
+++ backend.js
@@ -1,153 +1,67 @@
-//BACKEND.JS
-
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const controller = require('./controller/BibliotecaControllers');
+const controller = require('./controller/BibliotecaControllers.js');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(
-    express.static(
-        path.join(__dirname, 'Views')
-    )
-);
+app.use(express.static(path.join(__dirname, 'Views')));
 
 /* =========================
    LIBROS
 ========================= */
 
-app.get(
-    '/libros',
-    controller.getLibros
-);
-
-app.get(
-    '/libros/:id',
-    controller.getLibroById
-);
-
-app.post(
-    '/libros',
-    controller.crearLibro
-);
-
-app.put(
-    '/libros/:id',
-    controller.actualizarLibro
-);
-
-app.delete(
-    '/libros/:id',
-    controller.eliminarLibro
-);
+app.get('/libros', controller.getLibros);
+app.get('/libros/:id', controller.getLibroById);
+app.post('/libros', controller.crearLibro);
+app.put('/libros/:id', controller.actualizarLibro);
+app.delete('/libros/:id', controller.eliminarLibro);
 
 /* =========================
    LOGIN
 ========================= */
 
-app.post(
-    '/login',
-    controller.login
-);
+app.post('/login', controller.login);
 
-/* =========================
-   REGISTRO
-========================= */
-
-app.post(
-    '/register',
-    controller.register
-);
 
 /* =========================
    PRESTAMOS
 ========================= */
 
-app.post(
-    '/prestamos',
-    controller.crearPrestamo
-);
+app.post('/prestamos', controller.crearPrestamo);
 
-app.put(
-    '/devoluciones/:id',
-    controller.devolverPrestamo
-);
+app.get('/prestamos', controller.getPrestamos);
+
+/* DEVOLVER PRESTAMO */
+app.put('/prestamos/devolver/:libroId', controller.devolverPrestamo);
 
 /* =========================
    VISTAS
 ========================= */
 
 app.get('/', (req, res) => {
-
-    res.sendFile(
-        path.join(
-            __dirname,
-            'Views',
-            'login.html'
-        )
-    );
-
+    res.sendFile(path.join(__dirname, 'Views', 'login.html'));
 });
 
 app.get('/index', (req, res) => {
-
-    res.sendFile(
-        path.join(
-            __dirname,
-            'Views',
-            'Index.html'
-        )
-    );
-
+    res.sendFile(path.join(__dirname, 'Views', 'Index.html'));
 });
 
 app.get('/prestamo', (req, res) => {
-
-    res.sendFile(
-        path.join(
-            __dirname,
-            'Views',
-            'prestamo.html'
-        )
-    );
-
+    res.sendFile(path.join(__dirname, 'Views', 'prestamo.html'));
 });
 
 app.get('/devolucion', (req, res) => {
-
-    res.sendFile(
-        path.join(
-            __dirname,
- "/Views/devolucion.html"
-        )
-    );
-
+    res.sendFile(path.join(__dirname, 'Views', 'devolucion.html'));
 });
-
-
-app.get(
-    "/prestamos",
-    controller.getPrestamos
-);
-
-app.put(
-    '/prestamos/devolver/:libroId',
-    controller.devolverPrestamo
-);
-
 /* =========================
    SERVIDOR
 ========================= */
 
 app.listen(3000, () => {
-
-    console.log(
-        'Servidor corriendo en http://localhost:3000'
-    );
-
+    console.log('Servidor corriendo en http://localhost:3000');
 });
