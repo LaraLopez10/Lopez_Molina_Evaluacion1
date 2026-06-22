404ed8d35e0964f5ed9a73238d8a1347f15ff4a4
LaraLopez10 <larslopez20@gmail.com>
2026-06-22 16:51:17 -0300

    .

--- models/estrategias/adapterPrestamo.js
+++ models/estrategias/adapterPrestamo.js
@@ -1,23 +1,19 @@
 class LibroAdapter {
+  constructor(libro) {
+    this.libro = libro;
+  }
 
-    constructor(libro) {
-        this.libro = libro;
-    }
-
-    obtenerDatosPrestamo() {
-
-        return {
-            id: this.libro.id,
-            titulo: this.libro.titulo,
-            autor: this.libro.autor
-        };
-
-    }
-
+  obtenerDatosPrestamo() {
+    return {
+      id: this.libro.id,
+      titulo: this.libro.titulo,
+      autor: this.libro.autor,
+    };
+  }
 }
 
 module.exports = LibroAdapter;
 
-if (typeof module !== 'undefined') {
-    module.exports = LibroAdapter;
+if (typeof module !== "undefined") {
+  module.exports = LibroAdapter;
 }
